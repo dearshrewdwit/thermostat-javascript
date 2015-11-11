674210ce7664b4add01282f44a4d2f86a908db13
NULL-OPERATOR <x@cryptolab.net>
2015-11-11 11:26:25 +0000

    POWER SAVE

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -19,5 +19,32 @@ describe("thermostat logic", function() {
     expect(thermostat.getTemp()).toEqual(19);
   });
 
+  it("has a min temp", function() {
+    expect(thermostat.minTemp).toEqual(10);
+  });
 
+  it("can't go below 10", function() {
+    thermostat.temp = 10;
+    expect( function() {thermostat.downTemp();}).toThrowError("Min temp = 10");
+  });
+
+  it("can't go above 25", function() {
+    thermostat.temp = 25;
+    expect( function() {thermostat.upTemp();}).toThrowError("Max temp = 25");
+  });
+
+  it("the default mode of Power Save is true", function() {
+    expect(thermostat.mode).toBe(true);
+  });
+
+  it("power save mode off changes max temp to 32", function() {
+    thermostat.powerSaveMode();
+    expect(thermostat.maxTemp).toEqual(32);
+  });
+
+  it("power save mode on changes max temp to 25", function() {
+    thermostat.powerSaveMode();
+    thermostat.powerSaveMode();
+    expect(thermostat.maxTemp).toEqual(25);
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,8 @@
 function Thermostat() {
   this.temp = 20;
+  this.minTemp = 10;
+  this.maxTemp = 25;
+  this.mode = true;
 }
 
 Thermostat.prototype.getTemp = function() {
@@ -7,9 +10,26 @@ Thermostat.prototype.getTemp = function() {
 };
 
 Thermostat.prototype.upTemp = function() {
+  if (this.temp >= this.maxTemp) {
+     throw new Error("Max temp = "+ this.maxTemp);
+  }
   this.temp ++;
 };
 
 Thermostat.prototype.downTemp = function() {
+  if (this.temp <= this.minTemp) {
+     throw new Error("Min temp = " + this.minTemp);
+  }
   this.temp --;
+};
+
+Thermostat.prototype.powerSaveMode = function() {
+  if (this.mode) {
+    this.mode = false;
+    this.maxTemp = 32;
+  }
+  else {
+    this.mode = true;
+    this.maxTemp = 25;
+  }
 };
