e55beb4745e9d62838db341b87ab47cd293b4dde
dearshrewdwit <dearshrewdwit@gmail.com>
2015-11-11 10:50:13 +0000

    up and down TEMPERATURE

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,4 +8,16 @@ describe("thermostat logic", function() {
   it("has a starting temperature of 20", function(){
     expect(thermostat.getTemp()).toEqual(20);
   });
+
+  it("we can make it HOT", function() {
+    thermostat.upTemp();
+    expect(thermostat.getTemp()).toEqual(21);
+  });
+
+  it("we can make it COLD", function() {
+    thermostat.downTemp();
+    expect(thermostat.getTemp()).toEqual(19);
+  });
+
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,6 +2,14 @@ function Thermostat() {
   this.temp = 20;
 }
 
-Thermostat.prototype.getTemp = function(){
+Thermostat.prototype.getTemp = function() {
   return this.temp;
+};
+
+Thermostat.prototype.upTemp = function() {
+  this.temp ++;
+};
+
+Thermostat.prototype.downTemp = function() {
+  this.temp --;
 };
